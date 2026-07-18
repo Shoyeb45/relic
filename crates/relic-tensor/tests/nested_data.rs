114866fba0db90aa6240bbb76968691b3c6f4892
shoyeb45 <shoyebff45@gmail.com>
2026-07-18 10:41:54 +0530

    test(tensors): tensor creation and nested_data

---
+++ crates/relic-tensor/tests/nested_data.rs
@@ -0,0 +1,131 @@
+use relic_tensor::{NestedData, TensorError};
+
+// Happy Paths
+
+#[test]
+fn scalar_data() {
+    let val = 12.3;
+    assert_eq!(val.validate_shape(&mut Vec::new()), Ok(()));
+    assert_eq!(val.shape(), vec![]);
+}
+
+#[test]
+fn one_d_data() {
+    let vec = vec![12.2, 3.4, 12.0];
+    assert_eq!(vec.validate_shape(&mut Vec::new()), Ok(()));
+    assert_eq!(vec.shape(), vec![3]);
+}
+
+#[test]
+fn two_d_data() {
+    let vec = vec![vec![1.3, 3.4, 4.5], vec![3.4, 4.2, 2.3]];
+    assert_eq!(vec.validate_shape(&mut Vec::new()), Ok(()));
+    assert_eq!(vec.shape(), vec![2, 3]);
+}
+
+#[test]
+fn three_d_data() {
+    let vec = vec![
+        vec![vec![1.2, 0.3, 1.2], vec![1.3, 1.5, 1.5]],
+        vec![vec![1.2, 0.3, 1.2], vec![1.3, 1.5, 1.5]],
+    ];
+    assert_eq!(vec.validate_shape(&mut Vec::new()), Ok(()));
+    // 2 * 2 * 3
+    assert_eq!(vec.shape(), vec![2, 2, 3]);
+}
+
+#[test]
+fn four_d_data() {
+    let vec = vec![
+        vec![
+            vec![vec![1.1, 3.123], vec![1.4, 0.42]],
+            vec![vec![1.1, 3.123], vec![1.4, 0.42]],
+            vec![vec![1.1, 3.123], vec![1.4, 0.42]],
+        ],
+        vec![
+            vec![vec![1.1, 3.123], vec![1.4, 0.42]],
+            vec![vec![1.1, 3.123], vec![1.4, 0.42]],
+            vec![vec![1.1, 3.123], vec![1.4, 0.42]],
+        ],
+    ];
+    // 2 * 3 * 2 * 2
+    assert_eq!(vec.validate_shape(&mut Vec::new()), Ok(()));
+    assert_eq!(vec.shape(), vec![2, 3, 2, 2]);
+}
+
+#[test]
+fn one_d_data_arr() {
+    let vec = [12.2, 3.4, 12.0];
+    assert_eq!(vec.shape(), vec![3]);
+}
+
+#[test]
+fn two_d_data_arr() {
+    let vec = [[1.3, 3.4, 4.5], [3.4, 4.2, 2.3]];
+    assert_eq!(vec.shape(), vec![2, 3]);
+}
+
+#[test]
+fn three_d_data_arr() {
+    let vec = [
+        [[1.2, 0.3, 1.2], [1.3, 1.5, 1.5]],
+        [[1.2, 0.3, 1.2], [1.3, 1.5, 1.5]],
+    ];
+    // 2 * 2 * 3
+    assert_eq!(vec.shape(), vec![2, 2, 3]);
+}
+
+#[test]
+fn four_d_data_arr() {
+    let vec = [
+        [
+            [[1.1, 3.123], [1.4, 0.42]],
+            [[1.1, 3.123], [1.4, 0.42]],
+            [[1.1, 3.123], [1.4, 0.42]],
+        ],
+        [
+            [[1.1, 3.123], [1.4, 0.42]],
+            [[1.1, 3.123], [1.4, 0.42]],
+            [[1.1, 3.123], [1.4, 0.42]],
+        ],
+        [
+            [[1.1, 3.123], [1.4, 0.42]],
+            [[1.1, 3.123], [1.4, 0.42]],
+            [[1.1, 3.123], [1.4, 0.42]],
+        ],
+    ];
+    assert_eq!(vec.validate_shape(&mut Vec::new()), Ok(()));
+    assert_eq!(vec.shape(), vec![3, 3, 2, 2]);
+}
+
+#[test]
+fn ragged_two_d_1() {
+    let vec = vec![vec![1.1, 32.3, 1.1], vec![0.2, 1.2]];
+    // error at 2nd vector
+    assert_eq!(
+        vec.validate_shape(&mut Vec::new()),
+        Err(TensorError::RaggedShape {
+            path: vec![1],
+            expected: 3,
+            found: 2
+        })
+    );
+}
+
+#[test]
+fn ragged_two_d_2() {
+    let vec = vec![
+        vec![2.1, 2.12],
+        vec![1.2, 12.0, 12.3],
+        vec![1.2],
+        vec![1.5, 2.3],
+    ];
+    assert_eq!(
+        vec.validate_shape(&mut Vec::new()),
+        Err(TensorError::RaggedShape {
+            path: vec![1],
+            expected: 2,
+            found: 3
+        })
+    );
+}
--- crates/relic-tensor/tests/tensor.rs
+++ crates/relic-tensor/tests/tensor.rs
@@ -1,7 +1,94 @@
-use relic_tensor::Tensor;
+use relic_tensor::{DType, Tensor, TensorError};
 
 #[test]
-fn create_tensor() {
-    let tensor = Tensor::new(vec![2.3, 3.4]);
-    assert_eq!(tensor.len(), 2);
+fn scalar_tensor() {
+    let tensor_res: Result<Tensor, relic_tensor::TensorError> = Tensor::new(1.1, DType::F32);
+
+    assert!(tensor_res.is_ok());
+    let tensor = tensor_res.unwrap();
+
+    assert_eq!(*tensor.dims(), vec![]);
+    assert_eq!(*tensor.flatten(), vec![1.1]);
+    assert_eq!(format!("{}", tensor), "1.1");
 }
+
+#[test]
+fn one_d_tensor() {
+    let tensor_res = Tensor::new([1.12, 3.21, 2.3], DType::F32);
+    assert!(tensor_res.is_ok());
+    let tensor = tensor_res.unwrap();
+
+    assert_eq!(*tensor.dims(), vec![3]);
+    assert_eq!(*tensor.flatten(), [1.12, 3.21, 2.3]);
+    assert_eq!(format!("{}", tensor), "[1.1200, 3.2100, 2.3000]");
+}
+
+#[test]
+fn two_d_tensor() {
+    let tensor_res = Tensor::new(vec![vec![1.12, 3.21], vec![2.3, 1.0]], DType::F32);
+    assert!(tensor_res.is_ok());
+    let tensor = tensor_res.unwrap();
+
+    assert_eq!(*tensor.dims(), vec![2, 2]);
+    assert_eq!(*tensor.flatten(), [1.12, 3.21, 2.3, 1.0]);
+    assert_eq!(
+        format!("{}", tensor),
+        "[\n  [1.1200, 3.2100],\n  [2.3000, 1.0000]\n]"
+    );
+}
+
+#[test]
+fn three_d_tensor() {
+    let tensor_res = Tensor::new(
+        vec![
+            vec![vec![1.12, 3.21], vec![2.3, 1.0]],
+            vec![vec![1.12, 3.21], vec![12.3, 12.0]],
+            vec![vec![1.132, 3.21], vec![12.3, 12.0]],
+        ],
+        DType::F32,
+    );
+
+    assert!(tensor_res.is_ok());
+    let tensor = tensor_res.unwrap();
+
+    assert_eq!(*tensor.dims(), vec![3, 2, 2]);
+    assert_eq!(
+        *tensor.flatten(),
+        [
+            1.12, 3.21, 2.3, 1.0, 1.12, 3.21, 12.3, 12.0, 1.132, 3.21, 12.3, 12.0
+        ]
+    );
+    assert_eq!(
+        format!("{}", tensor),
+        "[
+  [
+    [1.1200, 3.2100],
+    [2.3000, 1.0000]
+  ],
+  [
+    [1.1200, 3.2100],
+    [12.3000, 12.0000]
+  ],
+  [
+    [1.1320, 3.2100],
+    [12.3000, 12.0000]
+  ]
+]"
+    );
+}
+
+#[test]
+fn tensor_err_2d() {
+    let tensor_res = Tensor::new(vec![vec![1.2, 1.3], vec![43.12]], DType::F32);
+    assert!(tensor_res.is_err());
+    // we will get error because of ragged length
+    assert_eq!(
+        tensor_res,
+        Err(TensorError::RaggedShape {
+            path: vec![1],
+            expected: 2,
+            found: 1
+        })
+    );
+}
+
